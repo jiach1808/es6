c97d969d4955fc1fa2a4ff61b603a5bfc2f502b0
wjc <2396554126@qq.com>
2019-12-19 20:13:37 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//export  输出操作 （生成模块）
+//import  引入操作 （引入模块）
+
+import {a} from './temp.js';
+
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/temp.js
@@ -0,0 +1,4 @@
+//生成模块
+//export  var name='吴佳创'
+
+export var a = 'jspang';
